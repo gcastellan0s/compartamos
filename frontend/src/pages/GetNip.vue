d4bfd9d268238020c28a34b6217ecbf31ce227c3
Gustavo Castellanos <xyz@Gustavos-MacBook-Pro.local>
2020-12-14 02:40:30 -0600

    Primer version

--- frontend/src/pages/GetNip.vue
+++ frontend/src/pages/GetNip.vue
@@ -9,8 +9,7 @@
         ) SOLICITUD DE NIP
       q-form(
         ref='form'
-        lazy-validation
-        @submit.stop.prevent='login'
+        @submit.stop.prevent='getNIP(form)'
       )
         q-card-section.q-pa-sm.row
           q-item.col-lg-4.col-md-4.col-sm-12.col-xs-12
@@ -25,8 +24,9 @@
                 outlined=''
                 round=''
                 mask='#### - #### - #### - ####'
-                placeholder='9999 - 9999 - 9999 - 9999'
+                placeholder='ej. 9999 - 9999 - 9999 - 9999'
                 hint='16 numeros de su tarjeta bancaria'
+                :rules="[val => !!val || 'Ingresa un numero de tarjeta', val => val.length === 25 || 'Ingresa un numero válido']"
               )
           q-item.col-lg-4.col-md-4.col-sm-12.col-xs-12
             q-item-section.text-right.text-weight-bold(
@@ -39,8 +39,9 @@
                 dense=''
                 outlined=''
                 round=''
-                placeholder='JUAN PEREZ R'
+                placeholder='ej .JUAN PEREZ R'
                 hint='Número tal como aparece en su tarjeta'
+                :rules="[val => !!val || 'Ingresa un nombre']"
               )
           q-item.col-lg-4.col-md-4.col-sm-12.col-xs-12
             q-item-section.text-right.text-weight-bold(
@@ -57,6 +58,7 @@
                 placeholder='MM/AAAA'
                 hint='Mes a 2 digitos / Año a 4 digitos'
                 style='max-width:250px'
+                :rules="[val => !!val || 'Ingresa una fecha', val => val.length === 7 || 'Ingresa una fecha válida']"
               )
           q-separator.q-my-md
           q-item.col-lg-4.col-md-4.col-sm-12.col-xs-12
@@ -73,20 +75,40 @@
                 mask='(##) #### ####'
                 placeholder='10 dígitos 55-5555-6666'
                 hint='Número al que se enviará el NIP'
+                :rules="[val => !!val || 'Ingresa un numero', val => val.length === 14 || 'Ingresa un numero válido']"
               )
           q-card-actions.q-mt-lg.full-width(align='right')
             q-btn.text-weight-bold(
               color='primary'
               size=''
-              @click='getNIP(form)'
+              type='submit'
             ) Solicitar contraseña
             q-space
             q-btn.text-weight-bold(
               color='grey'
               size=''
+              type='reset'
               @click='reset'
             ) Cancelar
-</template>
+            q-dialog(v-model='success', persistent='', transition-show='scale', transition-hide='scale')
+              q-card.bg-positive.text-white(style='width: 300px')
+                q-card-section
+                  .text-h6
+                    | Éxito!
+                q-card-section.q-pt-none
+                  | Enviamos un mensaje de texto a tu número de teléfono
+                q-card-actions.bg-white.text-positive(align='right')
+                  q-btn(flat='', label='OK', @click='reload' )
+            q-dialog(v-model='error', persistent='', transition-show='scale', transition-hide='scale')
+              q-card.bg-negative.text-white(style='width: 300px')
+                q-card-section
+                  .text-h6
+                    | Ooops!
+                q-card-section.q-pt-none
+                  | Algo no salió bien, por favor revisa tus datos
+                q-card-actions.bg-white.text-negative(align='right')
+                  q-btn(flat='', label='OK', v-close-popup='')
+    </template>
 
 <script>
 import { GETNIP } from '@/models/api.js'
@@ -101,11 +123,18 @@ export default {
         fecha_vencimiento_mes: '',
         fecha_vencimiento_ano: '',
         numero_movil: '(55) 3723 2182'
-      }
+      },
+      success: false,
+      error: false
     }
   },
   methods: {
+    validate () {
+      if (this.$refs.form.validate()) return 1
+      return 0
+    },
     getNIP (form) {
+      if (!this.validate()) return 0
       this.$axios
         .get(GETNIP, {
           params: {
@@ -117,10 +146,17 @@ export default {
           }
         })
         .then(({ data }) => {
-          console.log(data)
+          this.success = true
+        })
+        .catch(() => {
+          this.error = true
         })
     },
+    reload () {
+      location.reload()
+    },
     reset () {
+      this.success = false
       this.form = {
         numero_tarjeta: '',
         nombre: '',
@@ -129,6 +165,7 @@ export default {
         fecha_vencimiento_ano: '',
         numero_movil: ''
       }
+      this.$refs.form.resetValidation()
     }
   }
 }
